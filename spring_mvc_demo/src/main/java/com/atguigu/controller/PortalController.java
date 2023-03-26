e26dc14ed22879542ed85f04c030a62794ea66ec
QiuShui <1097922642@qq.com>
2023-03-26 17:15:52 +0800

    feat: 02 spring_mvc_demo 视图控制器

--- spring_mvc_demo/src/main/java/com/atguigu/controller/PortalController.java
+++ spring_mvc_demo/src/main/java/com/atguigu/controller/PortalController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PortalController {
 
-    @RequestMapping("/")
-    public String portal() {
-        return "index";
-    }
+//    @RequestMapping("/")
+//    public String portal() {
+//        return "index";
+//    }
 
 }
